92d6bda5c655e15f6771cae0d4cd7b01a5e78438
clarkjanndy <cjcasol22@gmail.com>
2023-11-11 03:19:36 +0800

    feat: add redirect

--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/v1/docs', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls'))
 ]
